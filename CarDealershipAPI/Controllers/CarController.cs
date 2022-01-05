0cd11ea57c0e6a5fd04939669a7e18a99eccc6f7
Richard <richardmoss@rocketmortgage.com>
2022-01-05 18:58:57 -0500

    First commit

---
+++ CarDealershipAPI/Controllers/CarController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarDealershipAPI.Controllers
+{
+    
+    [ApiController]
+    [Route("[controller]")]
+    public class CarController : ControllerBase
+    {
+        Inventory lot1 = new Inventory();
+
+        [HttpGet]
+        public List<Car> ListAllCars()
+        {
+            return lot1.ListOfCars;
+        }
+        [HttpGet("GetAllMakes/{make}")]
+        public List<Car> GetAllMakes(string make)
+        {
+            List<Car> cars = new List<Car>();
+            foreach(Car c in lot1.ListOfCars)
+            {
+                if(c.Make.Contains(make) || c.Make.ToLower() == make.ToLower())
+                {
+                    cars.Add(c);
+                }
+            }
+            return cars;
+        }
+        [HttpGet("GetAllColors/{color}")]
+        public List<Car> GetCarColor(string color)
+        {
+            List<Car> cars = new List<Car>();
+            foreach(Car c in lot1.ListOfCars)
+            {
+                if(c.Color.Contains(color) || c.Color.ToLower() == color.ToLower())
+                {
+                    cars.Add(c);
+                }
+            }
+            return cars;
+        }
+        [HttpGet("IndexOfCars/{index}")]
+        public string IndexOfCars(int index)
+        {
+            return lot1.ListOfCars[index].ToString();
+        }
+    }
+}
---
+++ CarDealershipAPI/Inventory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarDealershipAPI
+{
+    public class Inventory
+    {
+        public List<Car> ListOfCars { get; set; } = new List<Car>();
+        public Inventory()
+        {
+            ListOfCars.Add(new Car("Ford", "Bronco", 2021, "White"));
+            ListOfCars.Add(new Car("Ford", "F-150", 2020, "Black"));
+            ListOfCars.Add(new Car("Chevy", "Cruze", 2009, "Tan"));
+            ListOfCars.Add(new Car("Toyota", "Camry", 2005, "Gray"));
+            ListOfCars.Add(new Car("GMC", "Sierra", 2010, "Red"));
+            ListOfCars.Add(new Car("Nissan", "Ultima", 2007, "Yellow"));
+            ListOfCars.Add(new Car("Ford", "Fusion", 2020, "Black"));
+            ListOfCars.Add(new Car("Toyota", "Corolla", 2012, "White"));
+            ListOfCars.Add(new Car("Tesla", "Model S", 2018, "Red"));
+
+        }
+    }
+}
